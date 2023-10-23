f11c5a9e7294c123d811ffe7d4e7d89f1fe2e10c
kannamb <80481279+kannamb@users.noreply.github.com>
2023-10-24 01:01:59 +0530

    Create EmailVerification.java

---
+++ EmailVerification.java
@@ -0,0 +1,29 @@
+package com.PracticeProject;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class EmailVerification {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean flag=false;
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("raghav@gmail.com");
+		list.add("ajay@gmail.com");
+		list.add("paveen@gmail.com");
+		list.add("deepak@gmail.com");
+		System.out.println(list);
+		System.out.println("Enter the email?");	
+		Scanner sc =new Scanner(System.in);
+		String input=sc.next();
+		
+		for( int i=0;i<list.size();i++) {
+        if(list.contains(input)) {
+        	System.out.println("Valid email");
+        	flag =true;
+        	break;
+        }
+		}
+        if (flag==false) {
+        	System.out.println("Invalid email");
+        }
+  }
+}
