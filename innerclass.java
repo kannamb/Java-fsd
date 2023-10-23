b09eb3be604a8374ab3721d0117db42678b25a19
kannamb <80481279+kannamb@users.noreply.github.com>
2023-10-24 00:44:23 +0530

    Create innerclass.java

---
+++ innerclass.java
@@ -0,0 +1,14 @@
+package innerclass;
+abstract class AnonymousInnerClass {
+	   public abstract void display();
+	}
+	public class anonymousinnerclass {
+	public static void main(String[] args) {
+	AnonymousInnerClass i = new AnonymousInnerClass() {
+	         public void display() {
+	            System.out.println("anonymous Inner Class");
+	         } 
+	      };
+	      i.display();  
+	   }
+	}
