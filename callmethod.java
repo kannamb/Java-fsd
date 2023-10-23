6386c2f112d83f9f688e925dc9df125f99d3d058
kannamb <80481279+kannamb@users.noreply.github.com>
2023-10-24 00:23:25 +0530

    Create callmethod.java

---
+++ callmethod.java
@@ -0,0 +1,14 @@
+package assistedprojects;
+public class callMethod {
+int val=100;
+int operation(int val) {
+	val =val*10/100;
+	return(val);
+}
+public static void main(String args[]) {
+	callMethod d = new callMethod();
+	System.out.println("Before operation value of data is "+d.val);
+	d.operation(200);
+	System.out.println("After operation value of data is "+d.val);
+	}
+}
