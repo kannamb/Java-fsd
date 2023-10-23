53824543c55307c8424cee538f7edddec044c84c
kannamb <80481279+kannamb@users.noreply.github.com>
2023-10-24 00:49:15 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class Array {
+	
+
+		public static void main(String[] args) {
+
+		
+		int a[]= {1,2,6,4,5};                   //single-dimensional array
+		for(int i=0;i<5;i++) {
+		System.out.println("Elements of An array a: "+a[i]);
+		}
+
+
+		
+		int[][] b = {
+		            {5, 7, 8, 9}, 
+		            {2, 4, 6} };                     //multidimensional array
+		      
+		      System.out.println("\nLength of row 1 is : " + b[0].length);
+		      }
+		}
